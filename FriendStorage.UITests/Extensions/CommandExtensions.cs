0564c021e2db6d13c03ed5aa313249ce79e23e2c
wshuts <shutswilliam@gmail.com>
2024-12-10 19:05:33 -0600

    IsCanExecuteChangedFired

---
+++ FriendStorage.UITests/Extensions/CommandExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FriendStorage.UITests.Extensions
+{
+  public static class CommandExtensions
+  {
+    public static bool IsCanExecuteChangedFired(
+      this ICommand command,
+      Action action)
+    {
+      var fired = false;
+      command.CanExecuteChanged += (s, e) => fired = true;
+
+      action();
+
+      return fired;
+    }
+  }
+}
